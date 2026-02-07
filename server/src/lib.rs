19fd1747370d54ed46da0d3fbe0f12728837baaa
Rafał Klusek <klusek.raf@gmail.com>
2026-02-07 16:39:25 +0100

    lint

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,22 +1,24 @@
 use axum::{Router, response::IntoResponse, routing::get};
 
 pub async fn run_server() {
-  let router = Router::new()
-    .route("/", get(handler));
+  let router = Router::new().route("/", get(handler));
 
   let server_ipport = "0.0.0.0:8000";
   log::info!("Starting server on {server_ipport}");
 
   let listener = tokio::net::TcpListener::bind(server_ipport)
     .await
-  .expect("unable to acquire tcp listener");
+    .expect("unable to acquire tcp listener");
 
   axum::serve(listener, router)
-  .with_graceful_shutdown(shutdown_signal())
-    .await.expect("unable to start axum server");
+    .with_graceful_shutdown(shutdown_signal())
+    .await
+    .expect("unable to start axum server");
 }
 
-async fn handler() -> impl IntoResponse { "ok" }
+async fn handler() -> impl IntoResponse {
+  "ok"
+}
 
 async fn shutdown_signal() {
   use tokio::signal::unix::{SignalKind, signal};
