622b92f7d0bdf77eee3c036bfa63c7994411c342
Rafał Klusek <klusek.raf@gmail.com>
2026-02-10 02:09:19 +0100

    websocket handler

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,7 +1,20 @@
-use axum::{Router, response::IntoResponse, routing::get};
+use std::time::Duration;
+
+use axum::{
+  Router,
+  extract::{
+    WebSocketUpgrade,
+    ws::{Message, WebSocket},
+  },
+  response::IntoResponse,
+  routing::get,
+};
+use tokio::time::sleep;
 
 pub async fn run_server() {
-  let router = Router::new().route("/", get(handler));
+  let router = Router::new()
+    .route("/", get(handler))
+    .route("/ws", get(ws_handler));
 
   let server_ipport = "0.0.0.0:8000";
   log::info!("Starting server on {server_ipport}");
@@ -18,6 +31,19 @@ pub async fn run_server() {
 
 async fn handler() -> impl IntoResponse {
   "ok"
+}
+
+async fn ws_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
+  ws.on_upgrade(async |mut socket: WebSocket| {
+    tokio::spawn(async move {
+      let mut i: u64 = 0;
+      loop {
+        let _ = socket.send(Message::Text(format!("{}", i).into())).await;
+        i += 1;
+        sleep(Duration::from_secs(1)).await;
+      }
+    });
+  })
 }
 
 async fn shutdown_signal() {
