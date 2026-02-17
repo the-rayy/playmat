737634d30869e4cf6007b335eff627fc48205691
Rafał Klusek <klusek.raf@gmail.com>
2026-02-17 17:48:34 +0100

    return token

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,4 +1,5 @@
-use std::time::Duration;
+use std::{sync::Arc, time::Duration};
+use tokio::sync::{Mutex};
 
 use axum::{
   Router,
@@ -10,7 +11,7 @@ use axum::{
   routing::get,
 };
 use futures_util::{SinkExt, StreamExt};
-use shared::{ClientMessageEnvelope, ServerMessage, ServerMessageEnvelope};
+use shared::{ClientMessage, ClientMessageEnvelope, ServerMessage, ServerMessageEnvelope, SignInCredentials, SignInToken};
 use tokio::time::sleep;
 
 pub async fn run_server() {
@@ -35,25 +36,45 @@ async fn handler() -> impl IntoResponse {
   "ok"
 }
 
+async fn handle_signin(data: SignInCredentials) -> ServerMessage {
+  sleep(Duration::from_secs(3)).await;
+  ServerMessage::SignIn(SignInToken{
+    token: String::from("asd"),
+  })
+}
+
 async fn ws_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
   ws.on_upgrade(async |socket: WebSocket| {
     let (mut ws_sender, mut ws_receiver) = socket.split();
 
+    let sender = Arc::new(Mutex::new(ws_sender));
+
+    let ws_sender = sender.clone();
     tokio::spawn(async move {
       while let Some(msg) = ws_receiver.next().await {
         if let Message::Binary(x) = msg.unwrap() {
-          let env = ClientMessageEnvelope::from_bytes(&x);
+          let env = ClientMessageEnvelope::from_bytes(&x).unwrap();
           log::debug!("Received: {:?}", env);
+
+          let resp = match env.msg {
+            ClientMessage::SignIn(data) => handle_signin(data).await,
+            _ => unreachable!(),
+          };
+
+          let env = ServerMessageEnvelope::new(resp);
+          let _ = ws_sender.lock().await.send(Message::Binary(env.to_bytes().into())).await;
         }
       }
     });
+
+    let ws_sender = sender.clone();
     tokio::spawn(async move {
       let mut i: u64 = 0;
 
       loop {
         let env = ServerMessageEnvelope::new(ServerMessage::Empty);
-        let _ = ws_sender.send(Message::Binary(env.to_bytes().into())).await;
-        let _ = ws_sender.send(Message::Text(format!("{}", i).into())).await;
+        let _ = ws_sender.lock().await.send(Message::Binary(env.to_bytes().into())).await;
+        let _ = ws_sender.lock().await.send(Message::Text(format!("{}", i).into())).await;
         i += 1;
         sleep(Duration::from_secs(1)).await;
       }
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -72,4 +72,10 @@ pub struct SignInCredentials {
 #[derive(bitcode::Encode, bitcode::Decode, Debug)]
 pub enum ServerMessage {
   Empty,
+  SignIn(SignInToken),
+}
+
+#[derive(bitcode::Encode, bitcode::Decode, Debug)]
+pub struct SignInToken {
+  pub token: String,
 }
