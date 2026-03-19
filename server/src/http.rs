54db388af064a2ee272b0ea6ddb198ab77712ca1
Rafał Klusek <klusek.raf@gmail.com>
2026-03-19 02:06:39 +0100

    simple server

--- server/src/http.rs
+++ server/src/http.rs
@@ -1,2 +1,40 @@
-pub async fn run() {
+use axum::{Router, response::IntoResponse, routing::get};
+
+use thiserror::Error as Thiserror;
+
+#[derive(Thiserror, Debug)]
+pub enum Error {
+  #[error("")]
+  InitializationError(#[from] std::io::Error)
+}
+
+pub async fn run(ipport: &str) -> Result<(), Error> {
+  let router = Router::new()
+  .route("/", get(handler));
+
+  let listener = tokio::net::TcpListener::bind(ipport)
+    .await?;
+
+  axum::serve(listener, router)
+    .with_graceful_shutdown(shutdown_signal())
+    .await
+    .expect("axum serve failed, but this should never happen");
+
+  Ok(())
+}
+
+async fn handler() -> impl IntoResponse {
+  "ok"
+}
+
+async fn shutdown_signal() {
+  use tokio::signal::unix::{SignalKind, signal};
+  let mut sigint = signal(SignalKind::interrupt()).unwrap();
+  let mut sigterm = signal(SignalKind::terminate()).unwrap();
+
+  tokio::select! {
+      _ = sigint.recv() => {},
+      _ = sigterm.recv() => {},
+  }
+  log::info!("Shutdown signal received, starting graceful shutdown");
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,10 @@
+mod config;
+mod http;
+
 #[tokio::main]
 async fn main() {
   env_logger::init();
+  if let Err(e) = http::run(&config::SERVER_IP_PORT).await {
+    panic!("{}", e);
+  }
 }
