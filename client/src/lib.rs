75ac5bedf0928b1cfb718734348728360c7bc658
Rafał Klusek <klusek.raf@gmail.com>
2026-03-20 01:37:23 +0100

    client with log

---
+++ client/src/lib.rs
@@ -0,0 +1,20 @@
+mod engine;
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen::prelude::wasm_bindgen(start))]
+pub fn main() {
+  engine::logger::init();
+  // let ctx = Arc::new(Mutex::new(Context::default()));
+  // platform::logger::init();
+  // platform::runtime::init();
+  // let net_tx = net::init(ctx.clone());
+  //
+  // let mut winit_app = app::App::new(ctx.clone(), net_tx);
+  //
+  // let event_loop = EventLoop::new().expect("unable to initialize winit EventLoop");
+  // event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
+  // event_loop
+  //   .run_app(&mut winit_app)
+  //   .expect("EventLoop run failed");
+  //
+  log::info!("Hello world!");
+}
