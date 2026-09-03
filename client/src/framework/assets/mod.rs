31eed24b85209af0851b9596aea7e275cb45044a
Rafał Klusek <klusek.raf@gmail.com>
2026-09-03 18:33:32 +0200

    save letter atlas to debug texture

--- client/src/framework/assets/mod.rs
+++ client/src/framework/assets/mod.rs
@@ -26,12 +26,47 @@ impl Context {
   }
 
   pub fn foo(&self) {
+    let charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     let font = include_bytes!("Kenney Pixel.ttf") as &[u8];
     let font = fontdue::Font::from_bytes(font, fontdue::FontSettings::default()).unwrap();
-    let (met, bmp) = font.rasterize('A', 200.0);
-    image::save_buffer("foo.png", &bmp, met.width as u32, met.height as u32, image::ExtendedColorType::L8).unwrap();
 
+    let rasterized = charset
+      .chars()
+      .map(|c| font.rasterize(c, 200.0))
+      .collect::<Vec<(fontdue::Metrics, Vec<u8>)>>();
 
+    let total_width = rasterized.iter().map(|(met, _)| met.width).sum::<usize>();
+    let height = rasterized.iter().map(|(met, _)| met.height).max().unwrap();
+
+    let mut bmp = Vec::new();
+    bmp.resize(total_width * height, 0_u8);
+
+    let mut x_offset = 0;
+
+    for (met, glyph) in &rasterized {
+      let w = met.width;
+      let h = met.height;
+
+      for y in 0..h {
+        for x in 0..w {
+          let src = x + y * w;
+          let dst = x_offset + x + y * total_width;
+
+          bmp[dst] = glyph[src];
+        }
+      }
+
+      x_offset += w;
+    }
+
+    image::save_buffer(
+      "foo.png",
+      &bmp,
+      total_width as u32,
+      height as u32,
+      image::ExtendedColorType::L8,
+    )
+    .unwrap();
   }
 
   pub const fn get(&self) -> &HashMap<TextureKey, Texture> {
